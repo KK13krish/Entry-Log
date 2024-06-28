467d786fb4ed4947222b346b664cf61967835577
KK13krish <kkrishnasree137@gmail.com>
2024-06-28 11:05:36 +0530

    login screen

--- app/src/main/java/com/example/entrylog/MainActivity.java
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.entrylog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppCompatButton b1;
+    EditText e1,e2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        e1=(EditText) findViewById(R.id.username);
+        e2=(EditText) findViewById(R.id.pswrd);
+        b1=(AppCompatButton) findViewById(R.id.loginbtn);
 
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getname=e1.getText().toString();
+                String getpass=e2.getText().toString();
+                Toast.makeText(getApplicationContext(),getname+" "+getpass,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
